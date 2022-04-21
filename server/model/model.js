90181b6026c11a35cccc8d7c7823996b7b5b3e3e
soham1969 <sohamr94@gmail.com>
2022-04-21 23:04:50 +0530

    personal details in model.js

--- server/model/model.js
+++ server/model/model.js
@@ -20,3 +20,6 @@ module.exports = Userdb;
 // sudo service mongodb start // for starting mongoDB service
 
 //https://docs.docker.com/engine/install/linux-postinstall/
+
+//sudo nano /var/lib/dpkg/statoverride for editing system files save with control +o then enter and then control +x
+//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 new github link
